ba9de6131083dda2794eabea409ab7b4fb2c65e1
Mbonyimfura <mbonyimfuratresor@gmail.com>
2023-07-22 16:34:22 +0200

    Add functionality for del,toggle +/- and %

--- index.js
+++ index.js
@@ -23,30 +23,85 @@ function calculate() {
             return num1 * num2;
         case '/':
             return num1 / num2;
+        case '%':
+            return num1%num2;
         default:
             return NaN;
     }
 }
-
+function handlePlusMinus() {
+    if (!operator) {
+        if (operand1 !== '' && operand1 !== '-') {
+            operand1 = (parseFloat(operand1) * -1).toString();
+            display.value = operand1;
+        }
+    } else {
+        if (operand2 !== '' && operand2 !== '-') {
+            operand2 = (parseFloat(operand2) * -1).toString();
+            display.value = operand2;
+        }
+    }
+}
 buttons.forEach(button => {
     button.addEventListener('click', function (event) {
         event.preventDefault(); 
         const value = this.innerText;
+     
         if (!isNaN(parseFloat(value))) {
             if (!operator) {
-                operand1 += value;
+                if (operand1 === '0') {
+                    operand1 = value;
+                    display.value = value;
+                } else {
+                    operand1 += value;
+                    display.value += value;
+                }
             } else {
-                operand2 += value;
+                if (operand2 === '0') {
+                    operand2 = value;
+                    display.value += value;
+                } else {
+                    operand2 += value;
+                    display.value += value;
+                }
             }
-            display.value += value;
         } else if (value === '+' || value === '-' || value === 'x' || value === '/') {
             if (operand1 && !operand2) {
                 operator = value;
                 display.value += ` ${value} `;
             }
-        } else if (value === 'C') {
+        }
+        else if (value === '%') {
+            if (operand1 && !operator && !operand2) {
+                operand1 = parseFloat(operand1) / 100;
+                display.value = operand1;
+            }
+        } 
+        else if (value === '.') {
+            if (!operator) {
+                if (!operand1.includes('.')) {
+                    if (operand1 === '' || operand1 === '-'){
+                        operand1 += '0';
+                        display.value += '0';
+                    }
+                    operand1 += value;
+                    display.value += value;
+                }
+            } else {
+                if (!operand2.includes('.')) {
+                    if (operand2 === '' || operand2 === '-'){
+                        operand2 += '0';
+                        display.value += '0';
+                    }
+                    operand2 += value;
+                    display.value += value;
+                }
+            }
+        }
+         else if (value === 'C') {
             clear();
-        } else if (value === '=') {
+        }
+         else if (value === '=') {
             if (operand1 && operator && operand2) {
                 const result = calculate();
                 display.value = result;
@@ -56,15 +111,24 @@ buttons.forEach(button => {
             }
             
         }
-        else if (value === '.') {
-            if (!display.value.includes('.')) {
-                if (!operator) {
-                    operand1 += value;
-                } else {
-                    operand2 += value;
-                }
-                display.value += value;
-            }
+        else if (value === '+/-') {
+            handlePlusMinus();
         }
+        else if(value==='DEL'){
+         if(!operator){
+            operand1=operand1.slice(0,-1)
+            display.value=operand1
+         }else if(operand2){
+            operand2=operand2.slice(0,-1)
+            display.value=display.value.slice(0,-1)
+         }else if(operator&&!operand2){
+            operator='';
+            display.value = display.value.slice(0, -3); 
+         }
+
+
+        }
+      
+        
     });
 });
